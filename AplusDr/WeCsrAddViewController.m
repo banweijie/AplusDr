ccd28ccf86277d06047a7093dfcb74444c9f0313
Sths <stevenhtx1993@163.com>
2014-05-17 00:01:11 +0800

    现在搜索医生的结果列表会在滑到最下方时自动加载后面的内容了

--- AplusDr/WeCsrAddViewController.m
+++ AplusDr/WeCsrAddViewController.m
@@ -12,14 +12,20 @@
 
 @interface WeCsrAddViewController () {
     UITableView * sys_tableView;
-    NSArray * doctorList;
+    NSMutableArray * doctorList;
     UISearchBar * searchBar;
     UITextField * user_searchContent_input;
+    UIView * loadingViewContainer;
+    UIActivityIndicatorView * loadingView;
+    BOOL hasMore;
+    BOOL isWaiting;
 }
 
 @end
 
 @implementation WeCsrAddViewController
+
+#define defaultNum 10
 
 /*
  [AREA]
@@ -28,17 +34,24 @@
 - (void) tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
     cell.alpha = We_alpha_cell_general;;
     cell.opaque = YES;
+    NSLog(@"%d %d", indexPath.section, indexPath.row);
+    if (indexPath.section == [doctorList count] - 1) {
+        if (!isWaiting) {
+            isWaiting = YES;
+            if (hasMore) [self queryMoreDoctors:self];
+        }
+    }
 }
 // 欲选中某个Cell触发的事件
 - (NSIndexPath *)tableView:(UITableView *)tv willSelectRowAtIndexPath:(NSIndexPath *)path
 {
-    we_doctorViewing = doctorList[path.section];
-    [self performSegueWithIdentifier:@"CsrAdd_pushto_CsrDci" sender:self];
     return path;
 }
 // 选中某个Cell触发的事件
 - (void)tableView:(UITableView *)tv didSelectRowAtIndexPath:(NSIndexPath *)path
 {
+    we_doctorViewing = doctorList[path.section];
+    [self performSegueWithIdentifier:@"CsrAdd_pushto_CsrDci" sender:self];
     [tv deselectRowAtIndexPath:path animated:YES];
 }
 // 询问每个cell的高度
@@ -48,11 +61,20 @@
 }
 // 询问每个段落的头部高度
 - (CGFloat)tableView:(UITableView *)tv heightForHeaderInSection:(NSInteger)section {
-    return 7;
+    return 10;
 }
 // 询问每个段落的尾部高度
 - (CGFloat)tableView:(UITableView *)tv heightForFooterInSection:(NSInteger)section {
+    if (section == [doctorList count] - 1) {
+        if (hasMore) return 1 + 40 + 10 + self.tabBarController.tabBar.frame.size.height;
+        else return 1 + 10 + self.tabBarController.tabBar.frame.size.height;
+    }
     return 1;
+}
+//
+- (UIView *)tableView:(UITableView *)tv viewForFooterInSection:(NSInteger)section{
+    if (hasMore && section == [doctorList count] - 1) return loadingViewContainer;
+    return nil;
 }
 // 询问共有多少个段落
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tv {
@@ -71,30 +93,45 @@
     }
     UILabel * l1;
     UILabel * l2;
-    PAImageView *avatarView;
+    __block UIImageView *avatarView;
     [[cell imageView] setContentMode:UIViewContentModeCenter];
-    switch (indexPath.row) {
-        case 0:
+    if (indexPath.section == [doctorList count]) {
+        UIActivityIndicatorView * tmp = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+        [tmp setFrame:cell.frame];
+        [tmp startAnimating];
+        [cell.contentView addSubview:tmp];
+        cell.backgroundView = [[UIView alloc] init];
+        cell.backgroundColor = [UIColor clearColor];
+    }
+    else {
+        WeDoctor * doctor = doctorList[indexPath.section];
+        if (indexPath.row == 0) {
             cell.contentView.backgroundColor = We_background_cell_general;
             // l1 - user name
             l1 = [[UILabel alloc] initWithFrame:CGRectMake(75, 12, 240, 23)];
-            l1.text = [NSString stringWithFormat:@"%@ %@", doctorList[indexPath.section][@"name"], we_codings[@"doctorCategory"][doctorList[indexPath.section][@"category"]][@"title"][doctorList[indexPath.section][@"title"]]];
-            if ([l1.text isEqualToString:@""]) l1.text = @"尚未设置名称";
+            if ([doctor.userName isEqualToString:@""]) {
+                l1.text = @"尚未设置名称";
+            }
+            else {
+                l1.text = [NSString stringWithFormat:@"%@ %@", doctor.userName, we_codings[@"doctorCategory"][doctor.category][@"title"][doctor.title]];
+            }
             l1.font = We_font_textfield_zh_cn;
             l1.textColor = We_foreground_black_general;
             [cell.contentView addSubview:l1];
             // l2 - lastMsg - content
             l2 = [[UILabel alloc] initWithFrame:CGRectMake(75, 33, 240, 23)];
-            l2.text = [NSString stringWithFormat:@"%@ %@", doctorList[indexPath.section][@"hospital"][@"name"], doctorList[indexPath.row][@"section"][@"text"]];
+            l2.text = [NSString stringWithFormat:@"%@ %@", doctor.hospitalName, doctor.sectionName];
             l2.textColor = We_foreground_gray_general;
             l2.font = We_font_textfield_small_zh_cn;
             [cell.contentView addSubview:l2];
             // avatar
-            avatarView = [[PAImageView alloc]initWithFrame:CGRectMake(15, 9, 48, 48) backgroundProgressColor:[UIColor clearColor] progressColor:[UIColor lightGrayColor]];
-            [avatarView setImageURL:yijiarenAvatarUrl(doctorList[indexPath.section][@"avatar"]) placeholder:nil successCompletion:nil];
+            avatarView = [[UIImageView alloc] initWithFrame:CGRectMake(15, 9, 48, 48)];
+            [avatarView setImageWithURL:[NSURL URLWithString:yijiarenAvatarUrl(doctor.avatarPath)]];
+            avatarView.layer.cornerRadius = 24;
+            avatarView.clipsToBounds = YES;
             [cell.contentView addSubview:avatarView];
-            break;
-        case 1:
+        }
+        if (indexPath.row == 1) {
             cell.imageView.image = [UIImage imageNamed:@"docinfo-crowdfunding"];
             cell.textLabel.text = @"众筹项目名称";
             cell.textLabel.font = We_font_textfield_small_zh_cn;
@@ -102,8 +139,7 @@
             cell.detailTextLabel.text = @"￥10,000已筹 / 52赞";
             cell.detailTextLabel.font = We_font_textfield_small_zh_cn;
             cell.detailTextLabel.textColor = We_foreground_gray_general;
-        default:
-            break;
+        }
     }
     return cell;
 }
@@ -128,10 +164,12 @@
     bg.contentMode = UIViewContentModeCenter;
     [self.view addSubview:bg];
     
-    [self refreshDoctorList:self];
+    // get doctor list
+    hasMore = YES;
+    [self initialDoctorList:self];
     
     // sys_tableView
-    sys_tableView = [[UITableView alloc] initWithFrame:CGRectMake(5, 64 + 85, 310, self.view.frame.size.height - self.tabBarController.tabBar.frame.size.height - 64 - 85) style:UITableViewStyleGrouped];
+    sys_tableView = [[UITableView alloc] initWithFrame:CGRectMake(5, 64 + 85, 310, self.view.frame.size.height - 64 - 85) style:UITableViewStyleGrouped];
     sys_tableView.autoresizingMask = UIViewAutoresizingFlexibleHeight|UIViewAutoresizingFlexibleWidth;
     sys_tableView.delegate = self;
     sys_tableView.dataSource = self;
@@ -139,43 +177,25 @@
     sys_tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
     [self.view addSubview:sys_tableView];
     
+    loadingViewContainer = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 1 + 40 + 10 + self.tabBarController.tabBar.frame.size.height)];
+    
+    loadingView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+    [loadingView setFrame:CGRectMake(0, 5, 320, 40)];
+    [loadingView startAnimating];
+    [loadingViewContainer addSubview:loadingView];
+    
+    // bar background image
     UIImageView * barBackground = [[UIImageView alloc] initWithFrame:CGRectMake(0, 64, 320, 85)];
     barBackground.image = [UIImage imageNamed:@"bar"];
     [self.view addSubview:barBackground];
     
+    // search bar
     searchBar = [[UISearchBar alloc] initWithFrame:CGRectMake(0, 104, 320, 45)];
     searchBar.placeholder = @"搜索";
-    //searchBar.barTintColor = [UIColor colorWithRed:225 / 255.0 green:224 / 255.0 blue:219 / 255.0 alpha:1.0];
-    // transparency for UISearchBar
     searchBar.translucent = YES;
     searchBar.backgroundImage = [UIImage new];
     searchBar.scopeBarBackgroundImage = [UIImage new];
-    //searchBar.layer.borderWidth = 0.5f;
-    //searchBar.layer.borderColor = [UIColor colorWithRed:206 / 255.0 green:187 / 255.0 blue:181 / 255.0 alpha:1.0];
-    //[searchBar.layer ]
-    [searchBar setTranslucent:YES];// 设置是否透明
-    //[searchBar setShowsCancelButton:YES animated:YES];
-
-    /* UIImageView * searchImage = [[UIImageView alloc] initWithFrame:(5, 5, 16, 16)];
-    searchImage.image = [UIImage imageNamed:<#(NSString *)#>]
-    
-    user_searchContent_input = [[UITextField alloc] initWithFrame:CGRectMake(10, 120, 300, 26)];
-    [user_searchContent_input setLeftViewMode:UITextFieldViewModeAlways]
-    user_searchContent_input.leftView =
-    user_searchContent_input.backgroundColor = [UIColor whiteColor];
-    user_searchContent_input.font = We_font_textfield_zh_cn;
-    user_searchContent_input.clipsToBounds = YES;
-    user_searchContent_input.layer.cornerRadius = 3.0f;
-    [self.view addSubview:user_searchContent_input];*/
-    
-    //myView.backgroundColor = [UIColor clearColor];
-    UIToolbar* bgToolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, 320, 450)];
-    bgToolbar.barStyle = UIBarStyleDefault;
-    bgToolbar.backgroundColor = [UIColor colorWithRed:225 / 255.0 green:224 / 255.0 blue:219 / 255.0 alpha:1.0];
-    [bgToolbar setBackgroundImage:[UIImage new]
-                  forToolbarPosition:UIToolbarPositionAny
-                          barMetrics:UIBarMetricsDefault];
-    //[self.view addSubview:bgToolbar];
+    [searchBar setTranslucent:YES];
     
     [self.view addSubview:searchBar];
 }
@@ -186,19 +206,85 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (void)refreshDoctorList:(id)sender {
-    // Get patients
+- (void)queryMoreDoctors:(id)sender {
+    NSDictionary * parameters = @{@"from":[NSString stringWithFormat:@"%d", [doctorList count]]};
+    AFHTTPRequestOperationManager * manager = [AFHTTPRequestOperationManager manager];
+    [manager GET:yijiarenUrl(@"patient", @"searchDoctors") parameters:parameters
+         success:^(AFHTTPRequestOperation *operation, id HTTPResponse) {
+             NSString * errorMessage;
+             NSString *result = [HTTPResponse objectForKey:@"result"];
+             result = [NSString stringWithFormat:@"%@", result];
+             NSLog(@"Total amount : %@", HTTPResponse[@"info"]);
+             if ([result isEqualToString:@"1"]) {
+                 NSLog(@"%@", HTTPResponse[@"response"]);
+                 for (int i = 0; i < [HTTPResponse[@"response"] count]; i++) {
+                     WeDoctor * doctor = [[WeDoctor alloc] init];
+                     [doctor setHospitalName:[WeAppDelegate toString:HTTPResponse[@"response"][i][@"hospital"][@"name"]]];
+                     [doctor setSectionName:[WeAppDelegate toString:HTTPResponse[@"response"][i][@"section"][@"text"]]];
+                     [doctor setCategory:[WeAppDelegate toString:HTTPResponse[@"response"][i][@"category"]]];
+                     [doctor setTitle:[WeAppDelegate toString:HTTPResponse[@"response"][i][@"title"]]];
+                     [doctor setUserId:[WeAppDelegate toString:HTTPResponse[@"response"][i][@"id"]]];
+                     [doctor setAvatarPath:[WeAppDelegate toString:HTTPResponse[@"response"][i][@"avatar"]]];
+                     [doctor setUserName:[WeAppDelegate toString:HTTPResponse[@"response"][i][@"name"]]];
+                     [doctor setUserPhone:[WeAppDelegate toString:HTTPResponse[@"response"][i][@"phone"]]];
+                     [doctorList addObject:doctor];
+                 }
+                 if ([HTTPResponse[@"info"] integerValue] == [doctorList count]) hasMore = NO;
+                 NSLog(@"%d", [HTTPResponse[@"info"] integerValue]);
+                 [sys_tableView reloadData];
+                 isWaiting = NO;
+                 return;
+             }
+             if ([result isEqualToString:@"2"]) {
+                 NSDictionary *fields = [HTTPResponse objectForKey:@"fields"];
+                 NSEnumerator *enumerator = [fields keyEnumerator];
+                 id key;
+                 while ((key = [enumerator nextObject])) {
+                     NSString * tmp1 = [fields objectForKey:key];
+                     if (tmp1 != NULL) errorMessage = tmp1;
+                 }
+             }
+             if ([result isEqualToString:@"3"]) {
+                 errorMessage = [HTTPResponse objectForKey:@"info"];
+             }
+             if ([result isEqualToString:@"4"]) {
+                 errorMessage = [HTTPResponse objectForKey:@"info"];
+             }
+             NSLog(@"Response error: %@", errorMessage);
+         }
+         failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+             NSLog(@"Error: %@", error);
+         }
+     ];
+
+}
+
+- (void)initialDoctorList:(id)sender {
     NSDictionary * parameters = @{};
     AFHTTPRequestOperationManager * manager = [AFHTTPRequestOperationManager manager];
     [manager GET:yijiarenUrl(@"patient", @"searchDoctors") parameters:parameters
          success:^(AFHTTPRequestOperation *operation, id HTTPResponse) {
              NSString * errorMessage;
-             
              NSString *result = [HTTPResponse objectForKey:@"result"];
              result = [NSString stringWithFormat:@"%@", result];
+             NSLog(@"Total amount : %@", HTTPResponse[@"info"]);
              if ([result isEqualToString:@"1"]) {
                  NSLog(@"%@", HTTPResponse[@"response"]);
-                 doctorList = HTTPResponse[@"response"];
+                 doctorList = [[NSMutableArray alloc] init];
+                 for (int i = 0; i < [HTTPResponse[@"response"] count]; i++) {
+                     WeDoctor * doctor = [[WeDoctor alloc] init];
+                     [doctor setHospitalName:[WeAppDelegate toString:HTTPResponse[@"response"][i][@"hospital"][@"name"]]];
+                     [doctor setSectionName:[WeAppDelegate toString:HTTPResponse[@"response"][i][@"section"][@"text"]]];
+                     [doctor setCategory:[WeAppDelegate toString:HTTPResponse[@"response"][i][@"category"]]];
+                     [doctor setTitle:[WeAppDelegate toString:HTTPResponse[@"response"][i][@"title"]]];
+                     [doctor setUserId:[WeAppDelegate toString:HTTPResponse[@"response"][i][@"id"]]];
+                     [doctor setAvatarPath:[WeAppDelegate toString:HTTPResponse[@"response"][i][@"avatar"]]];
+                     [doctor setUserName:[WeAppDelegate toString:HTTPResponse[@"response"][i][@"name"]]];
+                     [doctor setUserPhone:[WeAppDelegate toString:HTTPResponse[@"response"][i][@"phone"]]];
+                     [doctorList addObject:doctor];
+                 }
+                 if ([HTTPResponse[@"info"] integerValue] == [doctorList count]) hasMore = NO;
+                 NSLog(@"%d", [HTTPResponse[@"info"] integerValue]);
                  [sys_tableView reloadData];
                  return;
              }
@@ -217,23 +303,10 @@
              if ([result isEqualToString:@"4"]) {
                  errorMessage = [HTTPResponse objectForKey:@"info"];
              }
-             UIAlertView *notPermitted = [[UIAlertView alloc]
-                                          initWithTitle:@"获取保健医列表失败"
-                                          message:errorMessage
-                                          delegate:nil
-                                          cancelButtonTitle:@"确定"
-                                          otherButtonTitles:nil];
-             [notPermitted show];
+             NSLog(@"Response error: %@", errorMessage);
          }
          failure:^(AFHTTPRequestOperation *operation, NSError *error) {
              NSLog(@"Error: %@", error);
-             UIAlertView *notPermitted = [[UIAlertView alloc]
-                                          initWithTitle:@"获取保健医列表失败"
-                                          message:@"未能连接服务器，请重试"
-                                          delegate:nil
-                                          cancelButtonTitle:@"确定"
-                                          otherButtonTitles:nil];
-             [notPermitted show];
          }
      ];
 }
