157edf5f3916d609dc529a815d792b2be6811db9
ejren <baifenghua@baifenghuamatoMacBook-Air.local>
2014-10-09 11:16:57 +0800

    修改语音消息路径

--- AplusDr/WeAppDelegate.h
+++ AplusDr/WeAppDelegate.h
@@ -223,6 +223,7 @@ weTargetView we_targetView;
 #define yijiarenAvatarUrl(fileName) [NSString stringWithFormat:@"%@%@%@", yijiarenImageServer, we_imagePaths[@"avatarPath"], fileName]
 #define yijiarenCertUrl(fileName) [NSString stringWithFormat:@"%@%@%@", yijiarenImageServer, we_imagePaths[@"certPath"], fileName]
 #define yijiarenImageUrl(fileName) [NSString stringWithFormat:@"%@%@%@", yijiarenImageServer, we_imagePaths[@"imagePath"], fileName]
+#define yijiarenAudioUrl(fileName) [NSString stringWithFormat:@"%@%@%@", yijiarenImageServer, we_imagePaths[@"audioPath"], fileName]
 #define yijiarenVideoUrl(fileName) [NSString stringWithFormat:@"%@%@%@", yijiarenImageServer, we_imagePaths[@"videoPath"], fileName]
 #define yijiarenShareURL(field) [NSString stringWithFormat:@"%@/crowdfunding.jsp?fundingId=%@", yijiarenServer, field]
 
